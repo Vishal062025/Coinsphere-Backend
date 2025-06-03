de5dd734e44fb501cf33462fb011b9239c94894d
Vishal <vishal@intelisync.ai>
2025-06-03 23:17:46 +0530

    resolved issue

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { PrismaClient } from '@prisma/client';
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 
 const prisma = new PrismaClient();
 
