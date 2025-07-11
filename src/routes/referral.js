866989acf80f0c6ace509cee078c4d81b7f5a529
Sangam0207 <ssrv2024@gmail.com>
2025-07-11 10:09:57 +0530

    feat: resolved merged conflicts

--- src/routes/referral.js
+++ src/routes/referral.js
@@ -2,15 +2,13 @@ import express from 'express';
 import { authMiddleware, authorizeRoles } from '../middleware/authMiddleware.js';
 import {
   approveClaimRewardRequest,
-  createClaimRewardRequest,
   handleReferral,
 } from '../controllers/referralController.js';
 
 const router = express.Router();
 
 router.get('/', authMiddleware, handleReferral);
-router.post('/claim', authMiddleware, createClaimRewardRequest); // authMiddleware,
-router.post('/approve-claim/:claimId',  approveClaimRewardRequest); // authMiddleware, authorizeRoles('ADMIN', 'SUPER_ADMIN'),
+router.post('/approve-claim/:claimId', authMiddleware, authorizeRoles('ADMIN', 'SUPER_ADMIN'),  approveClaimRewardRequest); 
 
 
 export default router;
